430a3deacbbb264143cb2c4f0925d13f15340e3f
Jon Leopard <jontron2000@gmail.com>
2018-05-01 23:30:55 +0200

    Further project staging. Linting and formatting set up.

---
+++ src/globalStyles.js
@@ -0,0 +1,18 @@
+import styledNormalize from 'styled-normalize';
+import { injectGlobal } from 'styled-components';
+
+const fontUrl = 'https://fonts.googleapis.com/css?family=Open+Sans:300,400';
+
+export default injectGlobal`
+  ${styledNormalize}
+
+  @font-face {
+    font-family:'Open Sans';
+    src: url(${fontUrl}),
+  }
+
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+
+`;
